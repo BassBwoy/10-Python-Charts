1e395ae5c06532dc0e537f25d9af73577626a8e5
David Wanyoike <wabass@proton.me>
2024-01-28 12:21:33 +0300

    first commit

---
+++ scatterplot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.randn(50)
+y = 2 * x + 1 + 0.1 * np.random.randn(50)
+
+plt.scatter(x,y)
+plt.title('Scatter Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
+
